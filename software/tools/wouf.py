aa505a8b873afa4f01e55d3eb92e0ba8512bfb0a
angorange <github@angorange.com>
2019-11-23 09:05:40 -0800

    Revert "Update wouf.py"
    This reverts commit 2cd5fecc4ba4d2c7ca2dd701d3dfb77294576a0a.

--- software/tools/wouf.py
+++ software/tools/wouf.py
@@ -1,18 +1,15 @@
 #wouf 2018 (py3.6.4)
 #http://site2wouf.fr
-#Pour créer lexique.txt
+#Pour créer dico.txt
+#Qui contient un lexique basé sur
+#ODS7
 #En scollant le site : listesdemots.net
-#requis :l beautifulsoup4 request html5lib
+#requis :l beautifulsoup4
 #(cmd : pip install beautifulsoup4)
-#(cmd : pip install request)
-#(cmd : pip install html5lib)
  
 import requests
 from bs4 import BeautifulSoup
-
-filename = "../../data/lexique.txt"
-
-fichier = open(filename, "w")
+fichier = open("dico.txt", "w")
  
 #   Initialisation:
 url="https://www.listesdemots.net/touslesmots"
@@ -30,8 +27,10 @@ for l in lesmots:
 print()
 print("page 1 : OK ("+str(len(lesmots))+")")
 totalmot=len(lesmots)
+fichier.close()
 #page 2 à 8998:
 for i in range(2,899):
+    fichier = open("dico.txt", "a")
     lurl=url+"page"+str(i)+".htm"
     ok=False
     while not ok:
@@ -52,9 +51,8 @@ for i in range(2,899):
     totalmot+=len(lesmots)
     print()
     print("page "+str(i)+" : OK ("+str(len(lesmots))+"/"+str(totalmot)+")")
-
-fichier.close()
-#fin du script:
+    fichier.close()
+#fin du scipt:
   
  
 print("mots :",totalmot)
